d57478697825cabeb4a7bf0e7ba490ce89d20dc1
Nishant Arora <nish54@gmail.com>
2019-05-13 05:57:24 -0400

    tons of updates

--- planner_app-master/planner/planner/SleepViewController.swift
+++ planner_app-master/planner/planner/SleepViewController.swift
@@ -1,6 +1,3 @@
-//  SleepViewController.swift
-//  Copyright © 2016 Zubin Gadhoke. All rights reserved.
-
 import UIKit
 import HealthKit
 
@@ -11,49 +8,66 @@ class SleepViewController: UIViewController {
     
     @IBOutlet var displayTimeLabel: UILabel!
     @IBOutlet weak var summaryButton: UIButton!
+    @IBOutlet weak var titleLabel: UIButton!
+    @IBOutlet weak var descriptionLabel: UITextView!
+    @IBOutlet weak var viewDataButton: UIButton!
     
-    var startTime = TimeInterval()
-    var timer:Timer = Timer()
-    var endTime: NSDate!
+    var initialTime = TimeInterval()
+    var timer: Timer = Timer()
+    var stopTime: NSDate!
     var alarmTime: NSDate!
-
+    var nightModeStatus = UserDefaults.standard.bool(forKey: "nightModeOn")
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        //This code will prompt the user to allow or deny the requested permissions
-        let read = Set([
-            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
-            ])
-        
-        let share = Set([
-            HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!
-            ])
+        if nightModeStatus {
+            self.view.backgroundColor = UIColor .black
+            displayTimeLabel.textColor = UIColor .white
+            summaryButton.backgroundColor = UIColor .darkGray
+            titleLabel.setTitleColor(UIColor.white, for: .normal)
+            summaryButton.setTitleColor(UIColor.white, for: .normal)
+            descriptionLabel.textColor = UIColor .white
+            viewDataButton.setTitleColor(UIColor.white, for: .normal)
+
+        } else {
+            self.view.backgroundColor = UIColor(red: 90/255, green: 210/255, blue: 255/255, alpha: 1)
+            displayTimeLabel.textColor = UIColor .black
+            summaryButton.backgroundColor = UIColor .white
+            titleLabel.setTitleColor(UIColor.black, for: .normal)
+            summaryButton.setTitleColor(UIColor.black, for: .normal)
+            descriptionLabel.textColor = UIColor .black
+            viewDataButton.setTitleColor(UIColor.black, for: .normal)
+
+        }
+            
+            
+        let read = Set([HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!])
+        let share = Set([HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis)!])
+        summaryButton.titleLabel?.textAlignment = NSTextAlignment.center
         
         self.healthStore.requestAuthorization(toShare: share, read: read) { (success, error) -> Void in
-            if success == false {
+            if !success {
                 NSLog("Permission Denied")
             }
         }
+        super.viewDidLoad()
     }
-    
     
     @IBAction func start() {
         summaryButton.setTitle("", for: .normal)
         alarmTime = NSDate()
+        
         if (!timer.isValid) {
-            let aSelector : Selector = #selector(SleepViewController.updateTime)
-            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: aSelector, userInfo: nil, repeats: true)
-            startTime = NSDate.timeIntervalSinceReferenceDate
+            let updater: Selector = #selector(SleepViewController.updateTime)
+            timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: updater, userInfo: nil, repeats: true)
+            initialTime = NSDate.timeIntervalSinceReferenceDate
         }
         key = 1
     }
     
-    
     @IBAction func stop() {
-        endTime = NSDate()
-        saveSleep()
-        getSleep()
+        stopTime = NSDate()
+        saveData()
+        fetchData()
         timer.invalidate()
         key = 0
     }
@@ -61,121 +75,84 @@ class SleepViewController: UIViewController {
     @objc func updateTime() {
         let currentTime = NSDate.timeIntervalSinceReferenceDate
         
-        //Find the difference between current time and start time.
-        var elapsedTime: TimeInterval = currentTime - startTime
+        var timeSlept: TimeInterval = currentTime - initialTime
         
-        //calculate the minutes in elapsed time.
-        let minutes = UInt8(elapsedTime / 60.0)
-        elapsedTime -= (TimeInterval(minutes) * 60)
+        let mins = UInt8(timeSlept / 60.0)
+        timeSlept -= (TimeInterval(mins) * 60)
         
-        //calculate the seconds in elapsed time.
-        let seconds = UInt8(elapsedTime)
-        elapsedTime -= TimeInterval(seconds)
+        let seconds = UInt8(timeSlept)
+        timeSlept -= TimeInterval(seconds)
         
-        //find out the fraction of milliseconds to be displayed.
-        let fraction = UInt8(elapsedTime * 100)
+        let milliseconds = UInt8(timeSlept * 100)
         
-        //add the leading zero for minutes, seconds and millseconds and store them as string constants
-        
-        let strMinutes = String(format: "%02d", minutes)
-        let strSeconds = String(format: "%02d", seconds)
-        let strFraction = String(format: "%02d", fraction)
-        
-        //concatenate minuets, seconds and milliseconds as assign it to the UILabel
-        displayTimeLabel.text = "\(strMinutes):\(strSeconds):\(strFraction)"
+        displayTimeLabel.text = "\(String(format: "%02d", mins)):\(String(format: "%02d", seconds)):\(String(format: "%02d", milliseconds))"
     }
 
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func saveSleep() {
-        // alarmTime and endTime are NSDate objects
+    func saveData() {
         if let sleepType = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
-            
-            // we create our new object we want to push in Health app
-            let object = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.inBed.rawValue, start: self.alarmTime as Date, end: self.endTime as Date)
-            
-            if key == 1 {
-                sleepTime += self.endTime.timeIntervalSince(self.alarmTime as Date)
-                let hours = Int(sleepTime) / 3600
-                let minutes = Int(sleepTime) / 60 % 60
-                let seconds = Int(sleepTime) % 60
-                let interval = String(format:"%02i:%02i:%02i", hours, minutes, seconds)
-                print("interval: \(interval)" )
-                summaryButton.setTitle("You Slept for a total of \(interval) today", for: .normal)
-                key = 0
+            if alarmTime != nil {
+                let object = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.inBed.rawValue, start: self.alarmTime as Date, end: self.stopTime as Date)
+                
+                if key == 1 {
+                    sleepTime += self.stopTime.timeIntervalSince(self.alarmTime as Date)
+                    let hours = Int(sleepTime) / 3600
+                    let minutes = Int(sleepTime) / 60 % 60
+                    let seconds = Int(sleepTime) % 60
+                    let interval = String(format:"%02i:%02i:%02i", hours, minutes, seconds)
+    //                print("interval: \(interval)" )
+                    summaryButton.setTitle("You slept for a total of \(interval) last night.", for: .normal)
+                    key = 0
+                }
+                
+                healthStore.save(object, withCompletion: { (success, error) -> Void in
+                    if error != nil {
+                        print("Error Saving Data")
+                        return
+                    }
+                    
+                    if success {
+                        print("Data Saved")
+                    } else {
+                        print("Unexpected Error")
+                    }
+                })
+                
+                let object2 = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.asleep.rawValue, start: self.alarmTime as Date, end: self.stopTime as Date)
+                
+                healthStore.save(object2, withCompletion: { (success, error) -> Void in
+                    if error != nil {
+                        print("Error Saving Data")
+                        return
+                    }
+                })
             }
-            
-            // at the end, we save it
-            healthStore.save(object, withCompletion: { (success, error) -> Void in
-                
-                if error != nil {
-                    // something happened
-                    return
-                }
-                
-                if success {
-                    print("data saved in HealthKit")
-                } else {
-                }
-                
-            })
-            
-            
-            let object2 = HKCategorySample(type:sleepType, value: HKCategoryValueSleepAnalysis.asleep.rawValue, start: self.alarmTime as Date, end: self.endTime as Date)
-            
-            healthStore.save(object2, withCompletion: { (success, error) -> Void in
-                if error != nil {
-                    // something happened
-                    return
-                }
-                
-//                if success {
-//                } else {
-//                }
-//
-            })
-            
         }
     }
     
-    
-    func getSleep() {
-        
-//        var sleepTime = 0.0
-        
-        
-        // definte the object
-        if let sleep = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
-            
-            // get recent data
+    func fetchData() {
+        if let data = HKObjectType.categoryType(forIdentifier: HKCategoryTypeIdentifier.sleepAnalysis) {
             let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
             
-            // query data
-            let query = HKSampleQuery(sampleType: sleep, predicate: nil, limit: 30, sortDescriptors: [sortDescriptor]) { (query, tmpResult, error) -> Void in
+            let query = HKSampleQuery(sampleType: data, predicate: nil, limit: 30, sortDescriptors: [sortDescriptor]) {
+                (query, temp, error) -> Void in
                 
-                // if error occured - exit
                 if error != nil {
+                    print("Error Fetching Data")
                     return
                 }
                 
-                if let result = tmpResult {
+                if let result = temp {
                     for item in result {
                         if let sample = item as? HKCategorySample {
                             let value = (sample.value == HKCategoryValueSleepAnalysis.inBed.rawValue) ? "InBed" : "Asleep"
-                            print("Healthkit sleep: \(sample.startDate) \(sample.endDate) - value: \(value)")
-//                            sleepTime += sample.endDate.timeIntervalSince(sample.startDate)
+                            print("Time Slept: \(sample.startDate) \(sample.endDate) - Value: \(value)")
                         }
                     }
                 }
             }
-            
-            //execute query
             healthStore.execute(query)
         }
+    }
         
 //        let time = sleepTime/30
 //        let hours = Int(time) / 3600
@@ -183,15 +160,12 @@ class SleepViewController: UIViewController {
 //        let seconds = Int(time) % 60
 //        let interval = String(format:"%02i:%02i:%02i", hours, minutes, seconds)
 //        summaryButton.setTitle("You Slept for \(sleepTime)", for: .normal)
-    }
+   
     
     @IBAction func viewData(_ sender: Any) {
         if #available(iOS 10.0, *) {
             UIApplication.shared.open(URL(string: "x-apple-health://")!)
-        } else {
         }
     }
-    
-
 }
 
